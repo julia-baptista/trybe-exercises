15a417d8c4d31df65e8e8d13aaf962c1bd8cb2d2
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-24 18:29:31 -0300

    Adicinando os arquivos do conteudo da aula

---
+++ exercises/4.4/conteudo/funcoes.js
@@ -0,0 +1,34 @@
+var statusCarro = "desligado";
+var aceleracao = 0;
+var rotacaoDoVolante = 0;
+
+
+function ligarDesligar() {
+  if(statusCarro === "desligado") {
+    statusCarro = "ligado";
+  } else {
+    statusCarro = "desligado";
+  }
+  return statusCarro;
+}
+
+function acelerar(incremento) {
+  aceleracao = aceleracao + incremento;
+  return "Acelerando a " + aceleracao + "m/s";
+}
+
+function frear(decremento) {
+  aceleracao = aceleracao - decremento;
+  return "Desacelerando para " + aceleracao + "m/s";
+}
+
+function girarVolante(anguloRotacao) {
+  rotacaoDoVolante = anguloRotacao;
+  return rotacaoDoVolante + "°";
+}
+
+
+
+
+
+
