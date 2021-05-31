7a3669ff3a1f543fc6c2f53b608ecf3bed7a7786
Julia Baptista <baptista.julia@yahoo.com.br>
2021-05-31 12:13:05 -0300

    Aula 5.3 - Javascript: Eventos

---
+++ exercises/5.3/conteudo/parte1.js
@@ -0,0 +1,30 @@
+// Parte I - O que é u, escutador de eventos?
+
+<button onclick="maisInfos();"> Mais informações</button>
+
+function maisInfos() {
+  let textoMaisInfo = document.getElementById('trybe-continua');textoMaisInfo.innerText = "Aqui você vai aprender Hard Skills, Soft Skills e várias outras habilidades!"
+}
+
+// Parte II
+
+// Link de eventos:
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// Os mais comuns são: click , change , mouseover e keyup .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
