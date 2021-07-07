fd161dd816047038e4e976b8e70313183a989151
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-07 09:43:47 -0300

    exercicios 9.1

---
+++ exercises/9.1/conteudo/2. operacoes-assincronas2.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+
+
+/*********************************************************
+Por que a validação referente ao primeiro código funciona, e a referente ao segundo não? O teste espera receber o array [1, 2, 3] , mas apenas recebe o [2, 3] . Isso ocorre por causa da função setTimeout . Ela é uma função assíncrona, que espera alguns milissegundos para executar a função passada a ela como parâmetro. No código acima, ela espera 3000 milissegundos (3 segundos) para executar o pushNumber(numbers, 1) .
+*********************************************************/
